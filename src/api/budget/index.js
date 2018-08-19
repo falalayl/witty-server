a338108c5cb1afde9efde1c3f189d41ee86453bf
Anthony <anthonyballugjr@gmial.com>
2018-08-20 04:09:47 +0800

    changed del route auth for testing

--- src/api/budget/index.js
+++ src/api/budget/index.js
@@ -15,7 +15,7 @@ router.get('/', auth.optional, controller.getEntries);
 router.get('/:id', auth.optional, controller.getEntry);
 router.post('/', auth.required, controller.create);
 router.put('/:id', auth.required, controller.update);
-router.delete('/:id', auth.required, controller.destroy);
+router.delete('/:id', auth.optional, controller.destroy);
 router.get('/:userId', auth.optional, controller.getOne);
 
 module.exports = router;
