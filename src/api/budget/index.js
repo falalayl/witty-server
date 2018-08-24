396d6e5c5d947f33b46e861822d2153a08c414f5
Anthony <anthonyballugjr@gmial.com>
2018-08-24 16:52:51 +0800

    changed optional auth to required

--- src/api/budget/index.js
+++ src/api/budget/index.js
@@ -15,7 +15,6 @@ router.get('/', auth.optional, controller.getEntries);
 router.get('/:id', auth.optional, controller.getEntry);
 router.post('/', auth.required, controller.create);
 router.put('/:id', auth.required, controller.update);
-router.delete('/:id', auth.optional, controller.destroy);
-router.get('/:userId', auth.optional, controller.getOne);
-
+router.delete('/:id', auth.required, controller.destroy);
+router.get('/user/:userId', auth.required, controller.getOne);
 module.exports = router;
--- src/api/wallets/index.js
+++ src/api/wallets/index.js
@@ -13,7 +13,7 @@ var auth = require('../../services/auth/jwt');
 
 router.get('/', auth.optional, controller.getEntries);
 router.get('/:id', auth.optional, controller.getEntry);
-router.get('/user/:user', auth.optional,controller.getMyWallets);
+router.get('/user/:user', auth.required,controller.getMyWallets);
 router.post('/', auth.required, controller.create);
 router.put('/:id', auth.required, controller.update);
 router.delete('/:id', auth.required, controller.destroy);
