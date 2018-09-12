cc3c9f16f9bf1585a0a164543bf98dcb5f63b0b9
Anthony <anthonyballugjr@gmial.com>
2018-09-12 11:03:00 +0800

    code cleanup

--- src/api/archives/archives.model.js
+++ src/api/archives/archives.model.js
@@ -35,41 +35,4 @@ var ArchiveSchema = new Schema({
         }
     });
 
-// ArchiveSchema
-//     .post('save', (doc) => {
-//         var data = {
-//             'id': doc._id,
-//             'totalSavings': doc.totalSavings,
-//             'totalExpenses': doc.totalExpenses,
-//             'totalBudget': doc.totalBudget
-//         }
-//         var csv = json2csv({
-//             data: data
-//         }).exec()
-//             .then(fs.writeFile(csvFile, csv, function (err) {
-//                 if (err) throw err;
-//                 console.log(csv);
-//                 res.send('done');
-//             }));
-//     });
-
-ArchiveSchema
-    .path('period')
-    .validate((value) => {
-        return this.constructor.findOne({ period: value })
-            .exec()
-            .then(archive => {
-                if (archive) {
-                    if (this._id === archive._id) {
-                        return true;
-                    }
-                    return false;
-                }
-                return true;
-            })
-            .catch(err => {
-                throw err;
-            });
-    }, 'Period Already exists, Please make sure that your saving your last Budget Profile');
-
 module.exports = mongoose.model('Archive', ArchiveSchema);
